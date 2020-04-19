7ff1b27601753eca6debaaf76e2e28dfffa62322
Tofik Mussa <tofahub@gmail.com>
2020-04-19 06:50:34 -0400

    hint logic working

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,27 +1,43 @@
 <template>
   <div>
     <b-card
-      title="Name something that is..."
+      title="Name a word fitting the descriptions below"
       body-class="text-center"
       header-tag="nav"
     >
-      <b-card-text>
-        With supporting text below as a natural lead-in to additional content.
+      <b-card-text v-if="!hintone && !hinttwo && !hintthree" class="hints"
+        >Sorry there is no hint available for the selected word
+        <b-button variant="danger" id="go-back">
+          <div class="spacer"></div>
+          <router-link to="/">Try again</router-link>
+        </b-button>
       </b-card-text>
 
-      <div class="row">
+      <b-card-text v-if="hintone" class="hints"
+        >Hint - {{ hintone }}</b-card-text
+      >
+      <b-card-text v-if="hinttwo" class="hints"
+        >Hint - {{ hinttwo }}</b-card-text
+      >
+      <b-card-text v-if="hintthree" class="hints"
+        >Hint - {{ hintthree }}</b-card-text
+      >
+
+      <div class="row" v-if="hintone || hinttwo || hintthree">
         <b-button variant="success" id="first-button">Submit Answer</b-button>
         <div class="spacer"></div>
-        <b-button variant="danger"
-          ><router-link to="/">Go back to hompage</router-link></b-button
-        >
+        <b-button variant="danger" id="go-back">
+          <router-link to="/">Select a different category</router-link>
+        </b-button>
       </div>
     </b-card>
   </div>
 </template>
 
 <script>
+require('dotenv').config();
 import wordData from '../../public/wordData';
+import axios from 'axios';
 /* eslint-disable no-unused-vars */
 export default {
   data: function() {
@@ -29,7 +45,9 @@ export default {
       wordData: wordData.data,
       randomWord: '',
       category: this.$route.params.category,
-      hint: ''
+      hintone: '',
+      hinttwo: '',
+      hintthree: '',
     };
   },
   methods: {
@@ -43,17 +61,44 @@ export default {
       }
     },
   },
-  mounted() {
+  created() {
     this.pickWord();
+    axios({
+      method: 'GET',
+      url: 'https://wordsapiv1.p.rapidapi.com/words/' + this.randomWord,
+      headers: {
+        'content-type': 'application/octet-stream',
+        'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
+        'x-rapidapi-key': process.env.VUE_APP_WORD_API_KEY,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        console.log(this.randomWord);
+        this.hintone = response.data.results[0].definition;
+        this.hinttwo = response.data.results[1].definition;
+        this.hintthree = response.data.results[2].definition;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
 
 <style scoped>
 .spacer {
-  margin-left: 50px;
+  margin-left: 100px;
 }
 #first-button {
   margin-left: 500px;
+}
+.hints {
+  font-style: italic;
+  font-weight: bold;
+  background: grey;
+}
+#go-back {
+  font-weight: bold;
 }
 </style>
