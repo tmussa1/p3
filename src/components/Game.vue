ee70c8ccee1cd2548563914492a1b7f734f204e4
Tofik Mussa <tofahub@gmail.com>
2020-04-19 07:46:14 -0400

    mostly working

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -4,10 +4,11 @@
       title="Name a word fitting the descriptions below"
       body-class="text-center"
       header-tag="nav"
+      class="card-top"
     >
       <b-card-text v-if="!hintone && !hinttwo && !hintthree" class="hints"
         >Sorry there is no hint available for the selected word
-        <b-button variant="danger" id="go-back">
+        <b-button variant="danger" id="go-back" @click="replayGame">
           <div class="spacer"></div>
           <router-link to="/">Try again</router-link>
         </b-button>
@@ -23,12 +24,21 @@
         >Hint - {{ hintthree }}</b-card-text
       >
 
-      <div class="row" v-if="hintone || hinttwo || hintthree">
-        <b-button variant="success" id="first-button">Submit Answer</b-button>
-        <div class="spacer"></div>
-        <b-button variant="danger" id="go-back">
-          <router-link to="/">Select a different category</router-link>
-        </b-button>
+      <div v-if="hintone || hinttwo || hintthree">
+        <b-form-input
+          v-model="text"
+          placeholder="Enter your answer"
+          class="col-md-4"
+        ></b-form-input>
+        <div class="row">
+          <b-button variant="success" id="first-button" @click="replayGame"
+            >Play again</b-button
+          >
+          <div class="spacer"></div>
+          <b-button variant="danger" id="go-back">
+            <router-link to="/">Select a different category</router-link>
+          </b-button>
+        </div>
       </div>
     </b-card>
   </div>
@@ -60,38 +70,45 @@ export default {
         }
       }
     },
+    populateHint: function() {
+      axios({
+        method: 'GET',
+        url: 'https://wordsapiv1.p.rapidapi.com/words/' + this.randomWord,
+        headers: {
+          'content-type': 'application/octet-stream',
+          'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
+          'x-rapidapi-key': process.env.VUE_APP_WORD_API_KEY,
+        },
+      })
+        .then((response) => {
+          console.log(response);
+          console.log(this.randomWord);
+          this.hintone = response.data.results[0].definition;
+          this.hinttwo = response.data.results[1].definition;
+          this.hintthree = response.data.results[2].definition;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    replayGame: function() {
+      this.pickWord();
+      this.populateHint();
+    },
   },
   created() {
     this.pickWord();
-    axios({
-      method: 'GET',
-      url: 'https://wordsapiv1.p.rapidapi.com/words/' + this.randomWord,
-      headers: {
-        'content-type': 'application/octet-stream',
-        'x-rapidapi-host': 'wordsapiv1.p.rapidapi.com',
-        'x-rapidapi-key': process.env.VUE_APP_WORD_API_KEY,
-      },
-    })
-      .then((response) => {
-        console.log(response);
-        console.log(this.randomWord);
-        this.hintone = response.data.results[0].definition;
-        this.hinttwo = response.data.results[1].definition;
-        this.hintthree = response.data.results[2].definition;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    this.populateHint();
   },
 };
 </script>
 
 <style scoped>
 .spacer {
-  margin-left: 100px;
+  margin-left: 125px;
 }
 #first-button {
-  margin-left: 500px;
+  margin-left: 455px;
 }
 .hints {
   font-style: italic;
@@ -100,5 +117,15 @@ export default {
 }
 #go-back {
   font-weight: bold;
+}
+input {
+  display: inline-block;
+  margin-top: 50px;
+}
+.row {
+  margin-top: 50px;
+}
+.card-top {
+  margin-bottom: 50px;
 }
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,8 +8,14 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
           <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Filter categories"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit"
+              >Search</b-button
+            >
           </b-nav-form>
         </b-navbar-nav>
       </b-collapse>
@@ -18,17 +24,17 @@
 </template>
 
 <script>
-import wordData from "../../public/wordData";
+import wordData from '../../public/wordData';
 
 /* eslint-disable no-unused-vars */
 export default {
   data: function() {
     return {
-      wordData: wordData.data
+      wordData: wordData.data,
     };
   },
   methods: {},
-  mounted() {}
+  mounted() {},
 };
 </script>
 
